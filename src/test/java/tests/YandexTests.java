5e2c09d83d00378e84b950b7b39b8fd39c334fdf
alexanderbogdanovgit config --global user.name alexanderbogdanovgit config --global user.name alexanderbogdanov <ironweed@list.ru>
2020-06-17 20:55:15 +0300

    initial

--- src/test/java/tests/YandexTests.java
+++ src/test/java/tests/YandexTests.java
@@ -1,9 +1,34 @@
 package tests;
 
-import static com.codeborne.selenide.Selenide.open;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Condition;
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import pages.YandexPage;
 
-public class YandexTests {
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static helpers.Env.*;
+
+
+@Epic("QA.GURU automation course")
+@Story("Yandex tests")
+@Tag("yandex")
+
+public class YandexTests extends TestBase {
+
+    @Test
+    @Description("Positive Search Test With PageObject")
     void SearchTest() {
-        open("http://yandex.ru");
+        YandexPage yandexPage = new YandexPage();
+        open(yaUrl);
+        yandexPage.searchInput(YandexPage.query);
+        yandexPage.verifyResultFound(YandexPage.searchResult);
     }
 }
