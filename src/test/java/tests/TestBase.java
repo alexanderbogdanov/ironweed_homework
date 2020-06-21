96986d52bab390bc82bba98ca21c6ae47f7fba7e
alexanderbogdanovgit config --global user.name alexanderbogdanovgit config --global user.name alexanderbogdanov <ironweed@list.ru>
2020-06-21 18:27:40 +0300

    allure fix

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,9 +1,11 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
 
@@ -13,6 +15,14 @@ public class TestBase {
     public static void setUp() {
 //        Configuration.headless = true;
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
+
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setCapability("enableVNC", true);
+//        capabilities.setCapability("enableVideo", true);
+//
+//        Configuration.browserCapabilities = capabilities;
+////        Configuration.remote = "https://" + System.getProperty("selenoid_url") + ":4444/wd/hub/";
+//        Configuration.remote = "https://selenoid.autotests.cloud:4444/wd/hub/";
     }
 
     @AfterEach
--- src/test/java/tests/YandexTests.java
+++ src/test/java/tests/YandexTests.java
@@ -5,16 +5,21 @@ import com.codeborne.selenide.Condition;
 import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Story;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import pages.YandexPage;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byName;
-import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static helpers.Env.*;
+import static io.qameta.allure.Allure.step;
 
 
 @Epic("QA.GURU automation course")
@@ -24,6 +29,7 @@ import static helpers.Env.*;
 public class YandexTests extends TestBase {
 
     @Test
+    @Disabled
     @Description("Positive Search Test With PageObject")
     void SearchTest() {
         YandexPage yandexPage = new YandexPage();
@@ -31,4 +37,19 @@ public class YandexTests extends TestBase {
         yandexPage.searchInput(YandexPage.query);
         yandexPage.verifyResultFound(YandexPage.searchResult);
     }
+
+    @Test
+    @Description("Positive Search Test With Steps")
+    @DisplayName("Positive Search")
+    void SearchTestWithSteps() {
+        step("Открываем главную Яндекса", () -> open("https://yandex.ru"));
+        step("Вводим запрос в строке поиска", () -> {
+            $(by("id", "text")).val("lepra").pressEnter();
+        });
+        step("Проверяем результаты поиска", () -> {
+            $("html").shouldHave(text("Лепрозорий: вход"));
+        });
+    }
 }
+
+
